835226eb740b697566965ff43f5259e69e5e5f10
zaidusyy <zaidsayyed07869@gmail.com>
2026-08-15 05:53:40 +0530

    chore: place MusicPlayer above FreedomFighters section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
         <>
             <SmoothScroll />
             <ScrollyCanvas />
-            <FreedomFighters />
             <MusicPlayer />
+            <FreedomFighters />
             <Colophon />
         </>
     );
